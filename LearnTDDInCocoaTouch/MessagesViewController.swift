860ef427f4fef1f27301a75ad6a962b41e286e37
Josh Justice <jjustice@bignerdranch.com>
2018-01-01 20:00:07 -0500

    Clear text field upon save
    We hook up an action to the save button to clear out the text field when it's tapped.
    
    Now our feature test makes it all the way to the final assertion. The test is looking for the new message to appear somewhere on screen, but it doesn't:
    
    Red: NoMatchingElementException. accessibilityLabel('New message')

--- LearnTDDInCocoaTouch/MessagesViewController.swift
+++ LearnTDDInCocoaTouch/MessagesViewController.swift
@@ -1,5 +1,9 @@
 import UIKit
 
 class MessagesViewController: UITableViewController {
-
+    @IBOutlet var messageTextField: UITextField!
+    
+    @IBAction func save() {
+        messageTextField.text = ""
+    }
 }
