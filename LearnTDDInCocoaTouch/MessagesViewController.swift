d3112a532cd4c19e7e9b9930c6130ee2b31f7d2b
Josh Justice <jjustice@bignerdranch.com>
2018-01-01 20:03:22 -0500

    Drive table view out of messages store
    Rather than storing the messages in a simple array in the view controller, let's separate out responsibilities by creating a MessageStore class. First, let's write the code we wish we had in the view controller. We create a MessageStore, use it to provide the count and message for each table cell. When Save is tapped, we create a new message.
    
    Because we haven't implemented this MessageStore, we get compiler errors. Let's take them one at a time:
    
    Red: Use of undeclared type 'MessageStore'

--- LearnTDDInCocoaTouch/MessagesViewController.swift
+++ LearnTDDInCocoaTouch/MessagesViewController.swift
@@ -1,9 +1,37 @@
 import UIKit
 
 class MessagesViewController: UITableViewController {
+    private var store: MessageStore!
+    
     @IBOutlet var messageTextField: UITextField!
     
     @IBAction func save() {
+        guard let text = messageTextField.text else { return }
+        store.create(text)
         messageTextField.text = ""
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store = MessageStore()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return store.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        let message = store.get(indexPath.row)
+        cell.textLabel?.text = message
+
+        return cell
     }
 }
