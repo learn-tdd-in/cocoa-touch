ca826cbc534120b0dafde4892e195a2ebd429ee6
Josh Justice <jjustice@bignerdranch.com>
2018-01-01 20:18:46 -0500

    Specify returning the same message added
    We add another unit test to confirm that the get() method returns the same message added via create().
    
    Inner Red: ("fake message") is not equal to ("test message text")
    
    Our unit test error confirms that we're currently returning a hard-coded message.

--- LearnTDDInCocoaTouchTests/Unit/MessageStoreTests.swift
+++ LearnTDDInCocoaTouchTests/Unit/MessageStoreTests.swift
@@ -18,4 +18,13 @@ class MessageStoreTests: XCTestCase {
         XCTAssertEqual(count, 1, "Expected store.count to be 1 but was \(count)")
     }
     
+    func testGetShouldReturnMessageAdded() {
+        let createdText = "test message text"
+        
+        store.create(createdText)
+        
+        let retrievedText = store.get(0)
+        XCTAssertEqual(retrievedText, createdText)
+    }
+    
 }
